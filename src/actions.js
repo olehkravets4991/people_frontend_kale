0797d01d8d3212f71d813687c54febd5f2ff65ec
Oleh <kravetsoleh4991@gmail.com>
2023-08-16 18:48:01 -0700

    frontend auth

--- src/actions.js
+++ src/actions.js
@@ -16,6 +16,7 @@ export const createAction = async ({request}) => {
     await fetch(`${baseUrl}/people`, {
         // tell fetch to make a post request
         method: 'POST',
+        credentials:"include",
         headers: {
             // tells our backend the data is JSON
             "Content-Type": "application/json"
@@ -25,7 +26,7 @@ export const createAction = async ({request}) => {
     })
 
     // redirect the user back to the frontend index route
-    return redirect('/')
+    return redirect('/dashboard')
 }
 
 export const updateAction = async ({request, params}) => {
@@ -43,6 +44,7 @@ export const updateAction = async ({request, params}) => {
     await fetch(`${baseUrl}/people/${id}`, {
         // tell fetch to make a put request
         method: 'PUT',
+        credentials:"include",
         // teel backend the data is JSON
         headers: {
             "Content-Type": "application/json"
@@ -60,9 +62,73 @@ export const deleteAction = async ({params}) => {
     // send a delete request to our backend API
     await fetch(`${baseUrl}/people/${id}`, {
         // tell fetch to make a delete request
+        credentials:"include",
         method: 'DELETE'
         // no headers or body required for delete requests
     })
     // redirect back to the frontend index route
-    return redirect('/')
+    return redirect('/dashboard')
+}
+
+export const signupAction = async ({request}) => {
+    // get the form data
+    const formData = await request.formData()
+    // build out the new user
+    const newUser = {
+        username: formData.get('username'),
+        password: formData.get('password')
+    }
+    // send the new user to our backend API
+    const response = await fetch(`${baseUrl}/signup`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newUser)
+    })
+
+    // check if status is 400 or more
+    if (response.status >= 400) {
+        // alert the details of error
+        alert(response.statusText)
+        // redirect back to the frontend signup route
+        return redirect('/signup')
+    }
+
+    // redirect back to the frontend login route
+    return redirect('/login')
+}
+
+export const loginAction = async ({request}) => {
+    // get the form data
+    const formData = await request.formData()
+    // build out the user
+    const user = {
+        username: formData.get('username'),
+        password: formData.get('password')
+    }
+    // send the user to our backend API
+    const response = await fetch(`${baseUrl}/login`, {
+        method: "POST",
+        credentials: "include",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(user)
+    })
+
+    // check if status is 400 or more
+    if (response.status >= 400) {
+        // alert the details of error
+        alert(response.statusText)
+        // redirect back to the frontend login route
+        return redirect('/login')
+    }
+
+    // store whether loggedIn in localStorage
+    localStorage.setItem('loggedIn', JSON.stringify({status: true}))
+
+     // return redirect('/')
+    // redirect back to the frontend index route
+    return redirect('/dashboard')
 }
---
+++ src/pages/Login.js
@@ -0,0 +1,16 @@
+import { Form } from "react-router-dom";
+
+function Login(props){
+    return <div>
+        <h1>Login Form</h1>
+        <Form action="/login" method="post">
+            <label>Username</label>
+            <input type="text" name="username" placeholder="username" />
+            <label>Password</label>
+            <input type="password" name="password" placeholder="password" />
+            <input type="submit" value="Login" />
+        </Form>
+    </div>
+}
+
+export default Login;
